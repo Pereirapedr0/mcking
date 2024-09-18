aec6094553c14d3a1493339b8533b7aaff8723ff
Pereirapedr0 <132013108+Pereirapedr0@users.noreply.github.com>
2024-09-18 15:01:45 -0300

    botão para remover ingredientes

--- src/routes/orders/localStorageRepository.js
+++ src/routes/orders/localStorageRepository.js
@@ -33,9 +33,32 @@ export const addIngredients = async ({ request }) => {
 
 export const removeIngredients = async ({ request }) => {
   const item = await request.json();
+  const { ingrediente, pedido } = item;
+  
+  const orders = JSON.parse(window.localStorage.getItem('order') || '[]');
+  console.log("Orders before:", orders); // Log para verificar o estado inicial
+
+  const removerIngrediente = orders.find((order) => order.name === pedido);
+  console.log("Removing ingredient from:", removerIngrediente); // Log para verificar o pedido encontrado
+  
+  if (removerIngrediente && removerIngrediente.ingredients[ingrediente] > 0) {
+    removerIngrediente.ingredients[ingrediente] -= 1; // Remove um ingrediente
+    console.log(`Ingredient ${ingrediente} decreased. New count: ${removerIngrediente.ingredients[ingrediente]}`);
+  } else {
+    console.log(`Ingredient ${ingrediente} cannot be decreased. Current count: ${removerIngrediente.ingredients[ingrediente]}`);
+  }
+  
+  const indice = orders.findIndex(order => pedido.name === order.name);
+  orders[indice] = removerIngrediente;
+
+  console.log("Orders after:", orders); // Log para verificar o estado após a atualização
+
+  window.localStorage.setItem('order', JSON.stringify(orders));
   
   return null;
 }
+
+
 
 //alterar preços
 //alterar quantidades
